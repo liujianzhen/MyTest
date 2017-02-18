ba5e69d72b294390882d5fe70a54637366064c9a
liujianzhen <1550040296@qq.com>
2017-02-18 17:01:26 +0800

    modify path

--- src/nio/Nio.java
+++ src/nio/Nio.java
@@ -7,16 +7,16 @@ import java.nio.channels.FileChannel;
 public class Nio {
 
 	public static void main(String[] args) throws Exception {
-		FileInputStream fis = new FileInputStream("G:/ifm workspace/Test/src/nio/nio.txt");
+		FileInputStream fis = new FileInputStream("G:/eclipseJavaEE/MyTest/src/nio/nio.txt");
 		FileChannel fc = fis.getChannel();
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		fc.read(buffer);
 		buffer.flip();//将指针移到buffer字节数组的开始位置
 		while(buffer.remaining()>0){
-			byte a = buffer.get();
-			System.out.print(a);
+			byte charVar = buffer.get();
+			System.out.print(charVar);
 			System.out.print("->");
-			System.out.println((char)a);
+			System.out.println((char)charVar);
 		}
 		fis.close();
 	}
