9c4668adaaebddb4a44df56e93487bf2a34bc952
liujianzhen <1550040296@qq.com>
2017-02-22 16:45:47 +0800

    update thread.communicate.wait.Test.java

--- src/thread/communicate/wait/Test.java
+++ src/thread/communicate/wait/Test.java
@@ -1,5 +1,16 @@
 package thread.communicate.wait;
-
+/**
+ * 如果是有多个加一和减一的线程，就会出现问题
+ * 因为有可能出现一个加一线程完成后唤醒的又是一个加一线程，
+ * 而此时该唤醒的加一线程直接往下运行，没有再进行 number 的值的判断，就可能同时加两次。
+ * 此时就应该讲 if 改为 while
+ * jdk1.5中，提供了多线程的升级解决方案：
+ * 将同步synchronized替换为显式的Lock操作，将Object类中的wait(), notify(),notifyAll()替换成了Condition对象，
+ * 该对象可以通过Lock锁对象获取; 一个Lock对象上可以绑定多个Condition对象，这样实现了本方线程只唤醒对方线程
+ * 见 package thread.communicate.lock
+ * @author liujianzhen
+ *
+ */
 public class Test
 {
 	public static void main(String[] args) throws InterruptedException
